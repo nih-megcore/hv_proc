47387e4acfbaa3a2e7bacebf894f95437edc4689
Jeff Stout <stoutjd@nih.gov>
2022-10-17 13:47:08 -0400

    ENH - build a test set from the HV dataset

---
+++ make_openneuro_testset.sh
@@ -0,0 +1,31 @@
+
+
+#mamba create -p /fast/PYTHON_envs/datalad conda-forge::datalad -y 
+conda activate datalad
+
+topdir=$(pwd)
+OpenNeuroDset=ds004215
+#datalad install https://github.com/OpenNeuroDatasets/${OpenNeuroDset}.git
+cd ${OpenNeuroDset}
+datalad get sub-ON02747/ses-01/meg/*
+datalad get sub-ON02747/ses-01/anat/*T1w.nii.gz
+
+#Resampled the data into a new folder
+cd sub-ON02747/ses-01/meg/
+mkdir downsampled
+for dset in *.ds; do newDs -resample 6 ${dset} downsampled/${dset}; done
+cp *.tsv *.json downsampled
+
+#Make new openneuro dataset
+new_bids=${topdir}/downsampled_openneuro
+deriv_dir=${new_bids}/derivatives/ctf_downsample
+mkdir -p ${deriv_dir}
+
+new_megdir=${deriv_dir}/sub-ON02747/ses-01/meg/
+new_mridir=${deriv_dir}/sub-ON02747/ses-01/anat
+mkdir -p ${new_megdir}
+mv downsampled/* ${new_megdir}
+rm -r downsampled
+
+
+
