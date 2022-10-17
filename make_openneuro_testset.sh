20d7d392541686917d129f8341cf9978b401a3f3
Jeff Stout <stoutjd@nih.gov>
2022-10-17 14:49:44 -0400

    Additional steps

--- make_openneuro_testset.sh
+++ make_openneuro_testset.sh
@@ -8,7 +8,7 @@ OpenNeuroDset=ds004215
 #datalad install https://github.com/OpenNeuroDatasets/${OpenNeuroDset}.git
 cd ${OpenNeuroDset}
 datalad get sub-ON02747/ses-01/meg/*
-datalad get sub-ON02747/ses-01/anat/*T1w.nii.gz
+datalad get sub-ON02747/ses-01/anat/*
 
 #Resampled the data into a new folder
 cd sub-ON02747/ses-01/meg/
@@ -20,6 +20,16 @@ cp *.tsv *.json downsampled
 new_bids=${topdir}/downsampled_openneuro
 deriv_dir=${new_bids}/derivatives/ctf_downsample
 mkdir -p ${deriv_dir}
+cp ${topdir}/${OpenNeuroDset}/{participants.json,LICENSE,dataset_description.json} ${new_bids}
+
+out_participantTsv=${new_bids}/participants.tsv 
+head -1 ${topdir}/${OpenNeuroDset}/participants.tsv > "${out_participantTsv}"
+cat ${topdir}/${OpenNeuroDset}/participants.tsv | grep sub-ON02747 >> ${out_participantTsv}
+
+readme_fname=${new_bids}/README
+echo "This is test data for the following project">>${readme_fname}
+echo "doi:10.18112/openneuro.ds004215.v1.0.1">>${readme_fname}
+echo "Downsampled datasets are in the derivatives/ctf_downsample/ folder" >> ${readme_fname}
 
 new_megdir=${deriv_dir}/sub-ON02747/ses-01/meg/
 new_mridir=${deriv_dir}/sub-ON02747/ses-01/anat
@@ -27,5 +37,8 @@ mkdir -p ${new_megdir}
 mv downsampled/* ${new_megdir}
 rm -r downsampled
 
-
+#Open neuro won't accept just derivatives
+cd $topdir
+cp -L -r ${OpenNeuroDset}/sub-ON02747 ${new_bids}
+rm -rf ${new_bids}/sub-ON02747/ses-01/{dwi,func,perf}
 
